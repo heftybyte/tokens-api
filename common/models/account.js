47367727a5db211c60e75bff931cfeed57540fa7
Daniel Onodje <danielonodje@gmail.com>
2017-10-05 04:54:19 -0600

    remove errant logging

--- common/models/account.js
+++ common/models/account.js
@@ -28,7 +28,6 @@ module.exports = function(Account) {
 
   Account.registerDevice = (data, cb) => {
     Account.create(data, (err, instance) => {
-      console.log(err, instance);
       if (err) {
         const error = new Error(err.message);
         error.status = 400;
