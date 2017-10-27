e4b11fcac958b6c0c678df1e4c2e099955778082
Daniel Onodje <danielonodje@gmail.com>
2017-10-27 10:25:19 +0100

    add image url to payload

--- common/models/token.js
+++ common/models/token.js
@@ -1,2 +1,25 @@
 module.exports = function(Token) {
+  Token.getTokens = (cb) => {
+    Token.find().then(tokens => {
+      tokens = tokens.map(({id, decimals, symbol, address}) =>({
+        id, decimals, symbol, address,
+        imageUrl: `/img/tokens/${symbol.toLowerCase()}.png`,
+      })).sort((a, b)=>a.symbol > b.symbol ? 1 : -1);
+      cb(null, tokens);
+    });
+  };
+
+  Token.remoteMethod('getTokens', {
+    http: {
+      path: '/',
+      verb: 'get',
+    },
+    description: 'fetch all tokens',
+    returns: {
+      root: true,
+      type: 'array',
+    },
+  });
+
+  Token.disableRemoteMethodByName('find');
 };
