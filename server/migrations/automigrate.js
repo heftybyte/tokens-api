44c9fe26cf206c401c338f704d6e800768758a4c
Omidiora Samuel <Samparsky@MACBOOKs-MacBook-Pro.local>
2017-11-16 16:19:05 +0100

    script fix

--- server/migrations/automigrate.js
+++ server/migrations/automigrate.js
@@ -7,10 +7,11 @@ redisClient.quit();
 const ds = server.dataSources.arangodbDs;
 const model = process.argv[2];
 
-if (!model) {
-	console.log('\nYou must specify a specify model name. Be careful, this given model\'s collection will be overwritten.\n')
-	process.exit()
-}
+// if (!model) {
+// 	console.log('\nYou must specify a specify model name. Be careful, this given model\'s collection will be overwritten.\n')
+// 	process.exit()
+// }
+
 const collections = model ?
 	[model] :
 	server.models().map((model)=>model.modelName);
