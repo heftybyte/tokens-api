8c2046bd7ddd8694b7f078fee694afd6e4205363
Omidiora Samuel <Samparsky@MACBOOKs-MacBook-Pro.local>
2017-11-18 15:26:51 +0100

    node  production

--- server/ssl-config.js
+++ server/ssl-config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const fs = require("fs");
 
 if (process.env.NODE_ENV === 'production' && process.env.PLATFORM == "digitalocean") {
+	console.log("reading file")
 	exports.privateKey = fs.readFileSync(path.join(__dirname, './private/privkey.pem')).toString();
 	exports.certificate = fs.readFileSync(path.join(__dirname, './private/fullchain.pem')).toString();
 }
