76bd1a39ce35adc0c9940bf5d086924f1ba74a7d
Omidiora Samuel <Samparsky@MACBOOKs-MacBook-Pro.local>
2017-11-20 10:27:18 +0100

    udpate env

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,12 +4,10 @@ echo "--- Running npm bild ----"
 npm run build
 echo "---- cd dist ----"
 cd dist/
-echo "----- copy env file -----"
-cp env/.env.example ./.env
-echo "--- npm auotupdate ---"
-node ./server/migrations/autoupdate.js
 echo " --- copy env --"
 cp /var/www/tokens-express/env/.env /var/www/tokens-express/current/dist/
+echo "--- npm auotupdate ---"
+node ./server/migrations/autoupdate.js
 echo "--- pm2 restart -----"
 pm2 startOrRestart deploy/ecosystem.config.js -i 4 --update-env
 
