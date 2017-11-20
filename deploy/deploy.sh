8630b8a2fd7b15bbc9f40d055daec871aaab0da0
Omidiora Samuel <Samparsky@MACBOOKs-MacBook-Pro.local>
2017-11-20 09:12:21 +0100

    copy env

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,8 +4,12 @@ echo "--- Running npm bild ----"
 npm run build
 echo "---- cd dist ----"
 cd dist/
+echo "----- copy env file -----"
+cp env/.env.example ./.env
 echo "--- npm auotupdate ---"
 node ./server/migrations/autoupdate.js
+echo " --- copy env --"
+cp /var/www/tokens-express/env/.env /var/www/tokens-express/current/dist/
 echo "--- pm2 restart -----"
 pm2 startOrRestart deploy/ecosystem.config.js -i 4 --update-env
 
