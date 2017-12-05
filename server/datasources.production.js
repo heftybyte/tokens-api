2b7faf9a738a45a759014cf1605b50bc6b177058
Chinedu Daniel <chinedudaniel7@gmail.com>
2017-12-05 20:13:41 +0100

    Create and configure datasource for ticker api

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -12,4 +12,62 @@ module.exports = {
     'name': 'arangodbDs',
     'connector': 'loopback-connector-arangodb',
   },
+  'TickerApiService': {
+    'name': 'TickerApiService',
+    'connector': 'rest',
+    'baseUrl': process.env.TICKER_API_URL,
+    'operations': [{
+      'template': {
+        'method': 'GET',
+        'url': '/price/now',
+        'query': {
+          'fsym': '{^fsym:string}',
+          'tsym': '{^tsym:string}'
+        }
+      },
+      'functions': {
+        'priceNow': ['fsym', 'tsym']
+      }
+    }, {
+      'template': {
+        'method': 'GET',
+        'url': '/prices/now',
+        'query': {
+          'fsyms': '{^fsyms:string}',
+          'tsyms': '{^tsyms:string}'
+        }
+      },
+      'functions': {
+        'pricesNow': ['fsyms', 'tsyms']
+      },
+    }, {
+      'template': {
+        'method': 'GET',
+        'query': {
+          'fsym': '{^fsym:string}',
+          'tsym': '{^tsym:string}',
+          'start': '{^start:number}',
+          'end': '{^end:number}'
+        },
+        'url': '/price/histday'
+      },
+      'functions': {
+        'priceHistorical': ['fsym', 'tsym', 'start', 'end']
+      }
+    }, {
+      'template': {
+        'method': 'GET',
+        'query': {
+          'fsyms': '{^fsyms:string}',
+          'tsyms': '{^tsyms:string}',
+          'start': '{^start:number}',
+          'end': '{^end:number}'
+        },
+        'url': '/prices/histday'
+      },
+      'functions': {
+        'pricesHistorical': ['fsyms', 'tsyms', 'start', 'end']
+      }
+    }]
+  },
 };
