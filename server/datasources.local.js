8a7b32f14be642d608e44d904f3bf8a7e3770e34
Daniel Chinedu <chinedudaniel7@gmail.com>
2017-12-08 20:21:44 +0100

    Create and configure datasource for ticker api (#78)
    * Create and configure datasource for ticker api
    
    * Change functions on the ticker model to url endpoints

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -12,4 +12,62 @@ module.exports = {
     'name': 'arangodbDs',
     'connector': 'loopback-connector-arangodb',
   },
+  'TickerApiService': {
+    'name': 'TickerApiService',
+    'connector': 'rest',
+    'baseUrl': process.env.TICKER_API_URL || 'http://localhost:3003',
+    'operations': [{
+      'template': {
+        'method': 'GET',
+        'url': '/price/now',
+        'query': {
+          'fsym': '{^fsym:string}',
+          'tsym': '{^tsym:string}'
+        }
+      },
+      'functions': {
+        'price/now': ['fsym', 'tsym']
+      }
+    }, {
+      'template': {
+        'method': 'GET',
+        'url': '/prices/now',
+        'query': {
+          'fsyms': '{^fsyms:string}',
+          'tsyms': '{^tsyms:string}'
+        }
+      },
+      'functions': {
+        'prices/now': ['fsyms', 'tsyms']
+      },
+    }, {
+      'template': {
+        'method': 'GET',
+        'query': {
+          'fsym': '{^fsym:string}',
+          'tsym': '{^tsym:string}',
+          'start': '{^start:number}',
+          'end': '{^end:number}'
+        },
+        'url': '/price/histday'
+      },
+      'functions': {
+        'price/historical': ['fsym', 'tsym', 'start', 'end']
+      }
+    }, {
+      'template': {
+        'method': 'GET',
+        'query': {
+          'fsyms': '{^fsyms:string}',
+          'tsyms': '{^tsyms:string}',
+          'start': '{^start:number}',
+          'end': '{^end:number}'
+        },
+        'url': '/prices/histday'
+      },
+      'functions': {
+        'prices/historical': ['fsyms', 'tsyms', 'start', 'end']
+      }
+    }]
+  },
 };
