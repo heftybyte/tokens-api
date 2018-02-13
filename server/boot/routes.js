971d9bfef63fab91ff9cfec384e16e74b012e064
Omidiora Samuel <Samparsky@MACBOOKs-MacBook-Pro.local>
2018-02-13 18:36:47 +0100

    fix recommended issues

--- server/boot/routes.js
+++ server/boot/routes.js
@@ -1,39 +1,39 @@
-// const { clientLogger }  = require('../../lib/logger');
-// const { COIN_MARKETCAP, getAllPrices }  = require('../../lib/price');
-// const _ = require('lodash');
+const { clientLogger }  = require('../../lib/logger');
+const { COIN_MARKETCAP, getAllPrices }  = require('../../lib/price');
+const _ = require('lodash');
 
-// module.exports = app => {
-//   let router = app.loopback.Router();
-//   app.post('/api/client-logs', (req, res) => {
-//     console.log(req.body)
-//     const {message, level} = req.body;
-//     let allowedLevel = [
-//       'debug',
-//       'info',
-//       'notice',
-//       'warning',
-//       'err',
-//       'crit',
-// 	    'alert',
-// 	    'emerg',
-//     ];
+module.exports = app => {
+  let router = app.loopback.Router();
+  app.post('/api/client-logs', (req, res) => {
+    console.log(req.body)
+    const {message, level} = req.body;
+    let allowedLevel = [
+      'debug',
+      'info',
+      'notice',
+      'warning',
+      'err',
+      'crit',
+	    'alert',
+	    'emerg',
+    ];
 
-//     if (!_.includes(allowedLevel, level)) {
-//       return res.status(401).json({message: 'Invalid Level'});
-//     }
-// 	  clientLogger(message, level);
-//     return res.status(200).json({message: 'Log complete'});
-//   });
+    if (!_.includes(allowedLevel, level)) {
+      return res.status(401).json({message: 'Invalid Level'});
+    }
+	  clientLogger(message, level);
+    return res.status(200).json({message: 'Log complete'});
+  });
 
-//   app.get('/fetch-prices', async (req, res)=>{
-//     try {
-//       await getAllPrices('USD', COIN_MARKETCAP, false)
-//       res.send(200)
-//     } catch(e) {
-//       console.log('fetch prices', e)
-//       res.send(400)
-//     }
-//   })
+  app.get('/fetch-prices', async (req, res)=>{
+    try {
+      await getAllPrices('USD', COIN_MARKETCAP, false)
+      res.send(200)
+    } catch(e) {
+      console.log('fetch prices', e)
+      res.send(400)
+    }
+  })
 
-//   app.use(router);
-// };
+  app.use(router);
+};
