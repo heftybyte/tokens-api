db286299cf0340ee83f47ac08f81a15bd8a7f063
Esco Obong <esco@hbyte.com>
2018-02-19 00:58:59 -0500

    share endpoint with og meta

--- server/boot/routes.js
+++ server/boot/routes.js
@@ -1,5 +1,6 @@
 const { clientLogger }  = require('../../lib/logger');
 const { COIN_MARKETCAP, getAllPrices }  = require('../../lib/price');
+const TOKENS = require('../../data/tokens');
 const _ = require('lodash');
 
 module.exports = app => {
@@ -35,5 +36,31 @@ module.exports = app => {
     }
   })
 
+  app.get('/share/:symbol', (req, res)=>{
+    const symbol = req.params.symbol.toUpperCase()
+    const { name, description } = TOKENS[symbol]
+    const image = `https://api.tokens.express/img/tokens/${symbol.toLowerCase()}.png`
+    const url = `https://www.tokens.express`
+    const appLink = `tokensexpress://token/${symbol}`
+    const appName = 'Tokens Express'
+    const packageName = 'express.tokens.tokens'
+    const appStoreId = 0
+    const ogMeta = `
+    <meta property="og:url"               content="${url}" />
+    <meta property="og:type"              content="article" />
+    <meta property="og:title"             content="${name} - ${symbol}" />
+    <meta property="og:description"       content="${description}" />
+    <meta property="og:image"             content="${image}" />
+
+    <meta property="al:ios:app_store_id"  content="${appStoreId}" />
+    <meta property="al:ios:url"           content="${appLink}" />
+    <meta property="al:ios:app_name"      content="${appName}" />
+     
+    <meta property="al:android:package"   content="${packageName}" />
+    <meta property="al:android:url"       content="${appLink}" />
+    <meta property="al:android:app_name"  content="${appName}" />
+    `
+    res.send(ogMeta)
+  })
   app.use(router);
 };
